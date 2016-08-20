391a0b3976f428c786e5543f43584a145f132e9c
MikeBordelon <374mike@gmail.com>
2016-08-19 20:02:43 -0700

    Set up database schema fix specs

--- server/schema.sql
+++ server/schema.sql
@@ -4,8 +4,22 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(20) NOT NULL,
+  message VARCHAR(140),
+  roomname VARCHAR(20),
+  PRIMARY KEY (id)
 );
 
+CREATE TABLE users (
+  /* Describe your table here.*/
+  username VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE  rooms (
+  /* Describe your table here.*/
+  name VARCHAR(20) NOT NULL
+);
 /* Create other tables and define schemas for them here! */
 
 
